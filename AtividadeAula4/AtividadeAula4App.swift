4276de8672584dd8f4e457d54d042aa55c324bb0
Rafael Lara <rafaelsilva.l1995@gmail.com>
2021-08-21 15:37:20 -0300

    Initial Commit

---
+++ AtividadeAula4/AtividadeAula4App.swift
@@ -0,0 +1,17 @@
+//
+//  AtividadeAula4App.swift
+//  AtividadeAula4
+//
+//  Created by Rafael Silva on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct AtividadeAula4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
